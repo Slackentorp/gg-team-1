89ecce3a1d39b19198a64c3a774e6dc779625381
Kristian Rokholm <krje13@student.aau.dk>
2017-10-31 15:42:19 +0100

    Made the camera trail after the moth, when outside specified distance

---
+++ Assets/Scripts/CameraTranslation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTranslation : MonoBehaviour {
+
+	[SerializeField, Tooltip("Specifies the object the camera follows")]
+	GameObject moth;
+	[SerializeField, Tooltip("Sets the distance that the camera can move away from moth")]
+	float maxDistance;
+	[SerializeField, Tooltip("Decides the speed of which the camera follow the moth")]
+	float followSpeed;
+	float difference; 
+	Camera mainCamera;
+	
+	void Start ()
+	{
+		mainCamera = Camera.main;
+	}
+
+	void LateUpdate ()
+	{
+		difference  = Vector3.Distance(mainCamera.transform.position, moth.transform.position);
+		AboveDistance();
+	}
+
+	void AboveDistance()
+	{
+		if (difference * maxDistance > maxDistance)
+		{
+			mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, moth.transform.position, followSpeed * Time.deltaTime);
+		}
+		else
+		{
+			mainCamera.transform.position = mainCamera.transform.position;
+		}
+	}
+
+}
