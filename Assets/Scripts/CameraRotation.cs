72dbe625deeb551984562e9c898b2b4b331e0fc0
Kristian Rokholm <krje13@student.aau.dk>
2017-10-31 10:17:28 +0100

    Added a script that allow yawing, and pitching of the camera using arrow keys

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+	[SerializeField, Tooltip("Controls the camera's turning speed.")]
+	private float cameraTurnSpeed;
+	Camera mainCamera;
+
+	void Start()
+	{
+		mainCamera = Camera.main;
+	}
+
+	private void FixedUpdate()
+	{
+		RotateCameraAroundSelf();
+	}
+
+
+	private void RotateCameraAroundSelf()
+	{
+		mainCamera.transform.Rotate(0f, Input.GetAxis("Horizontal") * cameraTurnSpeed, 0f, Space.World);
+		mainCamera.transform.Rotate(-Input.GetAxis("Vertical") * cameraTurnSpeed, 0f, 0f, Space.Self);
+	}
+
+}
+	
+	
+	
