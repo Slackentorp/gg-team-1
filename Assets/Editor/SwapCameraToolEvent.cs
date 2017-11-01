01709a3809fd00498cca8f4982dc196d021e9760
Kiril <minikibork4@abv.bg>
2017-11-01 11:54:22 +0100

    Inspection Camera tool. Works both in editmode and playmode. It has a swap camera function, reset to default(before it was moved) and new possition(on the new object that is created)(Isaac knows).

---
+++ Assets/Editor/SwapCameraToolEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[ExecuteInEditMode]
+public class SwapCameraToolEvent : ScriptableWizard {
+    private ArrayList sceneViews;
+    Quaternion qaty;
+    Vector3 vecy;
+    GameObject gameObjecty;
+    [MenuItem("InspectionCamera Tool/Toly")]
+    static void SelectAllOfTagWizard()
+    {
+        ScriptableWizard.DisplayWizard<SwapCameraToolEvent>("InspectionCamera Tool", "Swap Cameras", "Return back");//first and second buttons
+    }
+
+    public void OnWizardCreate()
+    {
+        qaty = Camera.main.transform.rotation;
+        vecy = Camera.main.transform.position;
+        GameObject newObject = new GameObject("CameraScriptHolder");
+        newObject.AddComponent<InspectionCameraTool>();
+    }
+
+    public void OnWizardOtherButton()
+    {
+        Camera.main.transform.rotation = qaty;
+        Camera.main.transform.position = vecy;
+        gameObjecty = GameObject.Find("CameraScriptHolder");
+        DestroyImmediate(gameObjecty);
+
+    }
+
+}
