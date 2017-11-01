0e8d89ea510d4201880f6098ac48b8f3515a17db
Christian Fæster <chr.faester@gmail.com>
2017-11-01 16:22:35 +0100

    Prelim rotation puzzle.

---
+++ Assets/Scripts/Puzzles/TouchRotation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchRotation : MonoBehaviour, ITouchInput
+{
+    private RotationPuzzleController puzzleController;
+    private float rotationAmount;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    puzzleController =
+	        transform.parent.GetComponent<RotationPuzzleController>();
+	    rotationAmount = puzzleController.GetRotationAmount();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTap(Touch finger)
+    {
+        print("Tap");
+        transform.rotation = Quaternion.AngleAxis(rotationAmount, transform.up);
+    }
+
+    public void OnTouchDown(Touch finger)
+    {
+    }
+
+    public void OnTouchUp(Touch finger)
+    {
+    }
+
+    public void OnToucHold(Touch finger)
+    {
+    }
+
+    public void OnTouchExit()
+    {
+    }
+
+    public void OnSwipe(Touch finger, TouchDirection direction)
+    {
+    }
+}
