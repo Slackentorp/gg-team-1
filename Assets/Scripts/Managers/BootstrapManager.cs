d57fc88844b6bc7c1609c052984cbaef51ade074
Christian Fæster <chr.faester@gmail.com>
2017-11-05 15:36:11 +0100

    Changed the way scenes are loaded at run time (such as with the art test scene)

--- Assets/Scripts/Managers/BootstrapManager.cs
+++ Assets/Scripts/Managers/BootstrapManager.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Gamelogic.Extensions;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class BootstrapManager : MonoBehaviour
+public class BootstrapManager : Singleton<BootstrapManager>
 {
     [SerializeField]
     private Camera gameCamera;
 
+    private Scene levelScene;
+
     // Use this for initialization
     void Start()
     {
-        Scene levelScene = new Scene();
+        levelScene = new Scene();
         for (int i = 0; i < SceneManager.sceneCount; i++)
         {
             if (!SceneManager.GetSceneAt(i).name.Equals("Bootstrap") &&
                 !SceneManager.GetSceneAt(i).name.Equals("SoundScape"))
             {
                 levelScene = SceneManager.GetSceneAt(i);
+                SceneManager.SetActiveScene(levelScene);
                 break;
             }
         }
@@ -58,4 +62,20 @@ public class BootstrapManager : MonoBehaviour
         }
         Start();
     }
+
+    public void ChangeLevelScene(string level)
+    {
+        StartCoroutine(AsyncChangeLevelScene(level));
+    }
+
+    private IEnumerator AsyncChangeLevelScene(string level)
+    {
+        AsyncOperation unload = SceneManager.UnloadSceneAsync(levelScene);
+        while (!unload.isDone) {
+            yield return null;
+        }
+        Start();
+    }
+
+
 }
