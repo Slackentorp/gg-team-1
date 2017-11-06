6151aad46a55c03375fe6fc9062185a4d43ca080
gglarsen <av_larsen@hotmail.com>
2017-11-06 17:18:20 +0100

    Adds Switching Individual LightMaps.

--- Assets/Scripts/Managers/LightMapSwitcher.cs
+++ Assets/Scripts/Managers/LightMapSwitcher.cs
@@ -4,6 +4,13 @@ using System.Collections.Generic;
 
 public class LightMapSwitcher : MonoBehaviour
 {
+    private int arrayTrack;
+    private int[] myLightMapIndex;
+    private Renderer lmRenderer;
+
+    [SerializeField]
+    private Texture2D[] customLights;
+
     [SerializeField]
     private Texture2D[] lightsOn;
     [SerializeField]
@@ -13,40 +20,103 @@ public class LightMapSwitcher : MonoBehaviour
     [SerializeField]
     private Texture2D[] lightsDirOff;
 
-    private LightmapData[] lightsOnMaps;
+    public LightmapData[] _lightsOnMaps;
 
-    private LightmapData[] lightsOffMaps;
+    public LightmapData[] _lightsOffMaps;
+
+    public LightmapData[] _currentData;
+
+    private GameObject[] allObjects;
 
     void Start()
     {
-        lightsOnMaps = new LightmapData[lightsOn.Length];
-        for (int i = 0; i < lightsOn.Length; i++)
-        {
-            lightsOnMaps[i] = new LightmapData();
-            lightsOnMaps[i].lightmapColor = lightsOn[i];
-            lightsOnMaps[i].lightmapDir = lightsDirOn[i];
-        }
+        LightMapAssinger();
 
-        lightsOffMaps = new LightmapData[lightsOff.Length];
-        for (int i = 0; i < lightsOff.Length; i++)
-        {
-            lightsOffMaps[i] = new LightmapData();
-            lightsOffMaps[i].lightmapColor = lightsOff[i];
-            lightsOffMaps[i].lightmapDir = lightsDirOff[i];
-        }
+        LightMapIndexFinder();
     }
 
     public void SetLightsOn()
     {
-        print(lightsOnMaps.Length);
         print("lights On!");
-        LightmapSettings.lightmaps = lightsOnMaps;
+        LightmapSettings.lightmaps = _lightsOnMaps;
     }
 
     public void SetLightsOff()
     {
         print("lights Off!");
-        LightmapSettings.lightmaps = lightsOffMaps;
+        LightmapSettings.lightmaps = _lightsOffMaps;
+    }
+
+    public void LightMapAssinger()
+    {
+        _lightsOnMaps = new LightmapData[lightsOn.Length];
+        for (int i = 0; i < lightsOn.Length; i++)
+        {
+            _lightsOnMaps[i] = new LightmapData();
+            _lightsOnMaps[i].lightmapColor = lightsOn[i];
+        }
+
+        for (int i = 0; i < lightsDirOn.Length; i++)
+        {
+            _lightsOnMaps[i].lightmapDir = lightsDirOn[i];
+        }
+
+        _lightsOffMaps = new LightmapData[lightsOff.Length];
+        for (int i = 0; i < lightsOff.Length; i++)
+        {
+            _lightsOffMaps[i] = new LightmapData();
+            _lightsOffMaps[i].lightmapColor = lightsOff[i];
+        }
+
+        for (int i = 0; i < lightsDirOff.Length; i++)
+        {
+            _lightsOnMaps[i].lightmapDir = lightsDirOff[i];
+        }
+    }
+
+    public void LightMapIndexFinder()
+    {
+        allObjects = FindObjectsOfType<GameObject>();
+        arrayTrack = 0;
+        myLightMapIndex = new int[allObjects.Length];
+        //print("arrayLengt: " + allObjects.Length);
+
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.GetComponent<Renderer>() != null)
+            {
+                lmRenderer = obj.GetComponent<Renderer>();
+                myLightMapIndex[arrayTrack] = lmRenderer.lightmapIndex;
+                //print("arrayIndex: " + lmRenderer.lightmapIndex);
+                arrayTrack++;
+            }
+        }
+    }
+
+    public void SwitchLightNr()
+    {
+        foreach (GameObject obj in allObjects)
+        {
+            if (obj.GetComponent<Renderer>() != null)
+            {
+                lmRenderer = obj.GetComponent<Renderer>();
+                if (lmRenderer.lightmapIndex == 0)
+                {
+                    _currentData = new LightmapData[3];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        _currentData[i] = new LightmapData();
+                        print("imHere");
+                        customLights = lightsOn;
+                        customLights[0] = lightsOff[0];
+                        _currentData[i].lightmapColor = customLights[i];
+                    }
+                    LightmapSettings.lightmaps = _currentData;
+                }
+                //myLightMapIndex[arrayTrack] = lmRenderer.lightmapIndex;
+                arrayTrack++;
+            }
+        }
     }
 
 }
