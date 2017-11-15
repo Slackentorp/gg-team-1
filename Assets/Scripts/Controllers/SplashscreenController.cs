54d37cf34d830874a34ca3f74b982b8b30d5de4b
Christian Fæster <chr.faester@gmail.com>
2017-11-15 15:33:49 +0100

    Initial splashscreen

---
+++ Assets/Scripts/Controllers/SplashscreenController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+/// <summary>
+/// Controller script that plays the splash screen in proper order, and shows the #Gandhi quote 
+/// </summary>
+public class SplashscreenController : MonoBehaviour
+{
+    private void Start() {
+        
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Playables/FadeTrack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+using UnityEngine.Playables;
+	[System.Serializable]
+	[TrackMediaType (TimelineAsset.MediaType.Script)]
+	[TrackClipType (typeof(FadePlayable))]
+	[TrackBindingType (typeof(CanvasGroup))]
+	[TrackColor (0.2f, 1f, 0)]
+	public class FadeTrack : TrackAsset
+	{
+		protected override Playable CreatePlayable (PlayableGraph graph, GameObject go, TimelineClip clip)
+		{
+			var mixer = ScriptPlayable<FadePlayableBehaviour>.Create (graph);
+
+			var content = go.GetComponent<PlayableDirector> ();
+			var canvasGroup = content.GetGenericBinding (this) as CanvasGroup;
+			var fadeAsset = clip.asset as FadePlayable;
+			mixer.GetBehaviour ().canvasGroup = canvasGroup;
+			mixer.GetBehaviour ().fadeType = fadeAsset.fadeType;
+
+			clip.displayName = fadeAsset.fadeType.ToString ();
+			return mixer;
+		}
+	}
