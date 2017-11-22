2813b79ebafaec5788f93cd085e325b3fdad1c2f
Kristian Rokholm <krje13@student.aau.dk>
2017-11-22 01:50:47 +0100

    Fixed discrepensies with th LoadState Scirpt

--- Assets/Scripts/GameController/States/LoadState.cs
+++ Assets/Scripts/GameController/States/LoadState.cs
@@ -20,14 +20,11 @@ public class LoadState : GameState
             gm.LightController.LoadLights();
         }
         gm.InputManager = new InputManager(gm.InputSettings, gm.GameCamera.GetComponent<Camera>());
-<<<<<<< HEAD
 		gm.mothBehaviour = new MothBehaviour(gm.Moth, gm.GameCamera.GetComponent<Camera>(), .4f, gm.MothChildCurve, gm.noiseReducer, gm.mothSpeedModifier);
-=======
-        gm.mothBehaviour = new MothBehaviour(gm.Moth, gm.GameCamera.GetComponent<Camera>(), .4f);
->>>>>>> development
         gm.mothSounds = new MothSounds(gm.GameCamera.transform, gm.mothBehaviour, gm.Moth.transform);
         gm.cameraHeading = gm.GameCamera.transform.position - gm.Moth.transform.position;
-        //     gm.GameCamera.SetTarget(gm.Moth.transform.position);
+		//     gm.GameCamera.SetTarget(gm.Moth.transform.position);
+		gm.SetState(new RunState(gm));
 
     }
 
