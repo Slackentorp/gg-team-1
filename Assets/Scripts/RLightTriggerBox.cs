3d09f43ed64bdb583e8eb1a933b6534bc87ae3a1
gglarsen <av_larsen@hotmail.com>
2017-11-29 14:53:04 +0100

    Adds a prefab for triggering realtimeLights on/off. Notes: Used for testing realtime lights at the end room.

---
+++ Assets/Scripts/RLightTriggerBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RLightTriggerBox : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] realTimeLight;
+    private bool lightState;
+
+    private void Start()
+    {
+        for (int i = 0; i < realTimeLight.Length; i++)
+        {
+            if (!lightState)
+            {
+                lightState = true;
+                realTimeLight[i].SetActive(true);
+            }
+            else if (lightState)
+            {
+                lightState = false;
+                realTimeLight[i].SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        for(int i = 0; i < realTimeLight.Length; i++)
+        {
+            if (!lightState)
+            {
+                lightState = true;
+                realTimeLight[i].SetActive(true);
+            }
+            else if (lightState)
+            {
+                lightState = false;
+                realTimeLight[i].SetActive(false);
+            }
+        }
+    }
+}
