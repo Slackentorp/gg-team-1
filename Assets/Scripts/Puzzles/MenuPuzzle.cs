c3c1dc86585e3abc4565a1db8ec75df41ef71640
Tobias Dahl Nielsen <tobiasdn@gmail.com>
2017-12-04 19:46:32 +0100

    Updates wwise and menupuzzle script

--- Assets/Scripts/Puzzles/MenuPuzzle.cs
+++ Assets/Scripts/Puzzles/MenuPuzzle.cs
@@ -128,6 +128,8 @@ public class MenuPuzzle : MonoBehaviour
 
     private void OnPuzzleSolved()
     {
+		AkSoundEngine.PostEvent("PUZZLE_SOLVED", currentPiece);
+
         if (OnFinished != null)
         {
             OnFinished();
